d12386126941b97e944474f12b9c7446b04ce569
Archer Reilly <1246506786@qq.com>
2016-07-03 23:46:02 +0800

    ParserJsonCNMARC still havent tested on the large data set

--- src/util.py
+++ src/util.py
@@ -14,17 +14,18 @@ def ParseJsonCNMARC(dictjson):
 
     # publisher
     if '210' in dictjson:
-    # if dictjson['210']:
-        BookInfo['publisher'] = dictjson['210']
+        if 'c' in dictjson['210']:
+            start = dictjson['210'].index('c') + 1
+            end = dictjson['210'].index('d')
+            BookInfo['publisher'] = dictjson['210'][start:end]
     else:
         BookInfo['publisher'] = None
 
 
     # binding
     if '010' in dictjson:
-    # if dictjson['010']:
         if 'b' in dictjson['010']:
-            start = dictjson['010'].index('b')
+            start = dictjson['010'].index('b') + 1
             if 'd' in dictjson['010']:
                 end = dictjson['010'].index('d')
                 BookInfo['binding'] = dictjson['010'][start:end]
@@ -35,26 +36,23 @@ def ParseJsonCNMARC(dictjson):
 
     # price
     if '010' in dictjson:
-    # if dictjson['010']:
         if 'd' in dictjson['010']:
-            start = dictjson['010'].index('d')
+            start = dictjson['010'].index('d') + 1
             BookInfo['price'] = dictjson['010'][start:]
     else:
         BookInfo['price'] = None
 
     # summary
     if '330' in dictjson:
-    # if dictjson['330']:
-        start = dictjson['330'].index('a')
+        start = dictjson['330'].index('a') + 1
         BookInfo['summary'] = dictjson['330'][start:]
     else:
         BookInfo['summary'] = None
 
     # isbn13
     if '010' in dictjson:
-    # if dictjson['010']:
         if 'a' in dictjson['010']:
-            start = dictjson['010'].index('a')
+            start = dictjson['010'].index('a') + 1
             end = start + 17
             if 'b' in dictjson['010']:
                 end = dictjson['010'].index('b')
@@ -67,9 +65,8 @@ def ParseJsonCNMARC(dictjson):
 
     # pages
     if '215' in dictjson:
-    # if dictjson['215']:
         if 'a' in dictjson['215']:
-            start = dictjson['215'].index('a')
+            start = dictjson['215'].index('a') + 1
             if 'c' in dictjson['215']:
                 end = dictjson['215'].index('c')
             elif 'd' in dictjson['215']:
@@ -84,19 +81,18 @@ def ParseJsonCNMARC(dictjson):
 
     # pubdate
     if '100' in dictjson:
-    # if dictjson['100']:
         if 'a' in dictjson['100']:
-            start = dictjson['100'].index('a')
-            end = dictjson['100'].index('d')
+            start = dictjson['100'].index('a') + 1
+            # end = dictjson['100'].index('d')
+            end = start + 8
             BookInfo['pubdate'] = dictjson['100'][start:end]
     else:
         BookInfo['pubdate'] = None
 
     # title
     if '200' in dictjson:
-    # if dictjson['200']:
         if 'a' in dictjson['200']:
-            start = dictjson['200'].index('a')
+            start = dictjson['200'].index('a') + 1
             end = dictjson['200'].index('b')
             BookInfo['title'] = dictjson['200'][start:end]
     else:
@@ -105,7 +101,29 @@ def ParseJsonCNMARC(dictjson):
     # translators[]
 
     # authors[]
+    BookInfo['authors'] = []
+    if '701' in dictjson:
+        if 'a' in dictjson['701']:
+            start = dictjson['701'].index('a') + 1
+            end = dictjson['701'].index('9')
+            BookInfo['authors'].append(dictjson['701'][start:end])
+
+    if '702' in dictjson:
+        if 'a' in dictjson['702']:
+            start = dictjson['702'].index('a') + 1
+            end = dictjson['702'].index('9')
+            BookInfo['authors'].append(dictjson['702'][start:end])
 
     # tags[]
+    # BookInfo['tags'] = []
+    if '606' in dictjson:
+        dictjson['606'] = dictjson['606'][2:]
+        # replace a, x, y, z, j
+        dictjson['606'] = dictjson['606'].replace('a', '#')
+        dictjson['606'] = dictjson['606'].replace('x', '#')
+        dictjson['606'] = dictjson['606'].replace('y', '#')
+        dictjson['606'] = dictjson['606'].replace('z', '#')
+        dictjson['606'] = dictjson['606'].replace('j', '#')
+        BookInfo['tags'] = dictjson['606'].split('#')
 
     return BookInfo
